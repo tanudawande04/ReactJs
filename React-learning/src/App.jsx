9562feedc209ddf337d4ac10e8e5b1c17162e828
tanudawande04 <tanudawande54@gmail.com>
2025-08-03 23:56:30 +0530

    first commit

---
+++ React-learning/src/App.jsx
@@ -0,0 +1,27 @@
+
+
+export const App = () => {
+
+  return (
+    <div>
+      <Thapatech />
+      <Thapatech />
+      <Thapatech />
+
+
+
+    </div>
+  )
+};
+export default App;
+
+
+
+const Thapatech = () => {
+  return (
+    <div>
+      <h1>tannu</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellat aspernatur inventore illum perferendis quisquam neque consectetur, dolorum sed officiis porro unde numquam, beatae suscipit. Qui pariatur nam enim nesciunt.</p>
+    </div>
+  )
+}
