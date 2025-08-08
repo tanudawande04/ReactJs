9ca28810ce3ff5983125bc0f41bda8449338b6b0
tanudawande04 <tanudawande54@gmail.com>
2025-08-08 23:41:32 +0530

    added react states

--- React-learning/src/components/Hooks/state.jsx
+++ React-learning/src/components/Hooks/state.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
+
 export const State = () => {
-    let value = 0;
+    // let value = 0;
+    // const handleButtonClick = () => {
+    //     value++;
+    //     console.log(value);
+
+
+    // }
+
+    const [count, setCount] = useState(0);
     const handleButtonClick = () => {
-        value++;
-        console.log(value);
-
-
+        setCount(() => count + 1);
     }
 
     return (
         <>
-            <h1>{value}</h1>
+            <h1>{count}</h1>
             <button onClick={handleButtonClick}>increment</button>
         </>
 
