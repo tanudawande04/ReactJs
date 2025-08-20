bbbd2ef24e30a4d60a9b0c0c53446db7afb17171
tanudawande04 <tanudawande54@gmail.com>
2025-08-20 17:44:54 +0530

    added pikachi api data and cards

--- React-learning/src/components/Hooks/useEffect/HowNotToFetchApi.jsx
+++ React-learning/src/components/Hooks/useEffect/HowNotToFetchApi.jsx
@@ -3,6 +3,8 @@ import './Pokemon.css';
 
 export const NotFetchapi = () => {
     const [apiData, setApiData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const Api = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
@@ -11,8 +13,13 @@ export const NotFetchapi = () => {
             .then((res) => (res.json()))
             .then((data) => {
                 setApiData(data)
+                setLoading(false);      //promise ke through data handled hai.....
             })
-            .catch((error) => console.log(error));
+            .catch((error) => {
+                console.log(error);
+                setError(error);
+                setLoading(false);
+            });
     }
 
     useEffect(() => {
@@ -22,30 +29,61 @@ export const NotFetchapi = () => {
 
     console.log(apiData);
 
-    if (apiData) {
+    if (loading) {
         return (
-
-            <div className="containet">
-                <header>
-                    <h1>Let's Catch pokemon</h1>
-                </header>
-                <ul className="card-demo">
-                    <li className="pokemon-card">
-                        <figure>
-                            <img src={apiData.sprites.other.dream_world.front_default} alt={apiData.name} className="pokemon-img" />
-
-
-                        </figure>
-                        <h1>{apiData.name}</h1>
-                    </li>
-
-                </ul>
-
-
-
+            <div>
+                <h1>Loading.....</h1>
             </div>
-
         )
     }
 
+    if (error) {
+        return (
+            <div>
+                <h1>Error : {error.message}</h1>
+            </div>
+        )
+    }
+
+    //if (apiData) {
+    return (
+
+        <div className="containet">
+            <header>
+                <h1>Let's Catch pokemon</h1>
+            </header>
+            <ul className="card-demo">
+                <li className="pokemon-card">
+                    <figure>
+                        <img src={apiData.sprites.other.dream_world.front_default} alt={apiData.name} className="pokemon-img" />
+
+
+                    </figure>
+                    <h1>{apiData.name}</h1>
+                    <div className="greed-three-cols">
+                        <p className="pokemon-info">
+                            Weight : <span>{apiData.weight}</span>
+
+                        </p>
+
+                        <p className="pokemon-info">
+                            Height : <span>{apiData.height}</span>
+
+                        </p>
+
+                        <p className="pokemon-info">
+                            speed : <span>{apiData.stats[5].base_stat}</span>
+                        </p>
+                    </div>
+                </li>
+
+            </ul>
+
+
+
+        </div>
+
+    )
 }
+
+
